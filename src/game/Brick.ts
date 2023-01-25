0c300f519b57e356b69f674a4163459e46305931
Dino <dinopranjic92@gmail.com>
2023-01-25 13:46:57 -0500

    Scaffold Brick class

--- src/game/Brick.ts
+++ src/game/Brick.ts
@@ -1,5 +1,46 @@
 import { Position } from "~/types";
 
 export class Brick {
+  private brickImage: HTMLImageElement = new Image();
+
+  constructor(
+    private brickWidth: number,
+    private brickHeight: number,
+    private position: Position,
+    private brickHitPoints: number,
+    image: string
+  ) {
+    this.brickWidth = brickWidth;
+    this.brickHeight = brickHeight;
+    this.position = position;
+    this.brickHitPoints = brickHitPoints;
+    this.brickImage.src = image;
+  }
+
+  get width(): number {
+    return this.brickWidth;
+  }
+
+  get height(): number {
+    return this.brickHeight;
+  }
+
+  get pos(): Position {
+    return this.position;
+  }
+
+  get image(): HTMLImageElement {
+    return this.brickImage;
+  }
+
+  get hp(): number {
+    return this.brickHitPoints;
+  }
+
+  set hp(points: number) {
+    this.brickHitPoints = points;
+  }
+
+  
 
 };
