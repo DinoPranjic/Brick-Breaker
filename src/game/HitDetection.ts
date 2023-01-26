e42eabd8251a79a66a5f012bb1f4037b3e9c985f
Dino <dinopranjic92@gmail.com>
2023-01-25 20:07:54 -0500

    Add brick hit check functions

--- src/game/HitDetection.ts
+++ src/game/HitDetection.ts
@@ -22,9 +22,39 @@ export class HitDetection {
     }
     // checks for hit against ceiling
     if (
-      ball.pos.y < 0 + ball.height
+      ball.pos.y < 0
     ) {
       ball.changeYDirection();
     }
+  }
+
+  brickHit(ball: Ball, brick: Brick): boolean {
+    if (
+      ball.pos.x < brick.pos.x + brick.width &&
+      ball.pos.x + ball.width > brick.pos.x &&
+      ball.pos.y < brick.pos.y + brick.height && 
+      ball.pos.y + ball.height > brick.pos.y
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  checkBricks(ball: Ball, bricks: Brick[]): boolean {
+    let hit = false;
+
+    bricks.forEach((brick, i) => {
+      if(this.brickHit(ball, brick)) {
+        ball.changeYDirection();
+
+        if (brick.hp === 1) {
+          bricks.splice(i, 1);
+        } else {
+          brick.hp -= 1;
+        }
+        hit = true;
+      }
+    });
+    return hit;
   }
 }
